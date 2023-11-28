1ef33ef27fa799d46fb7a609df9a729280c5899c
lukepadiachy <148848073+lukepadiachy@users.noreply.github.com>
2023-11-28 12:27:53 +0200

    First Commit

---
+++ UserFeedBackPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UserFeedBackPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" What is your name? ");
+            string name = Console.ReadLine();
+            Console.Write(" favorite color? ");
+            string colour = Console.ReadLine();
+
+
+            // Allows Users to put in infomartion that will print to the screen
+
+
+            Console.WriteLine("Hello " + name + " your favorite color is " + colour);
+
+        }
+    }
+}
